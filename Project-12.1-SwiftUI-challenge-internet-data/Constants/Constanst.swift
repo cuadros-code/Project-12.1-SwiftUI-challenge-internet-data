d7b6202e9541009b5cb6939f516fcbf725dd510d
kevin cuadros <kevindavidcuadros@gmail.com>
2025-01-28 16:41:04 -0500

    create constants control and get data from internet

---
+++ Project-12.1-SwiftUI-challenge-internet-data/Constants/Constanst.swift
@@ -0,0 +1,14 @@
+//
+//  Constanst.swift
+//  Project-12.1-SwiftUI-challenge-internet-data
+//
+//  Created by Kevin Cuadros on 28/01/25.
+//
+
+import Foundation
+
+class Constanst {
+    
+    static let usersURL = "https://www.hackingwithswift.com/samples/friendface.json"
+    
+}
--- Project-12.1-SwiftUI-challenge-internet-data/ContentView.swift
+++ Project-12.1-SwiftUI-challenge-internet-data/ContentView.swift
@@ -8,15 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var users: [UserModel] = [UserModel]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(users, id: \UserModel.id) { user in
+                    Text(user.name)
+                }
+            }
+            
+            .navigationTitle("Contacts")
         }
-        .padding()
+        
+        .task {
+            await getUsers()
+        }
     }
+    
+    
+    func getUsers() async {
+        if users.count >= 1 {
+            return
+        }
+        do {
+            guard let url = URL(string: Constanst.usersURL) else {
+                return
+            }
+            
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodeUsers = try? JSONDecoder().decode(
+                [UserModel].self,
+                from: data
+            ) {
+                users = decodeUsers
+            } else {
+                print("Error decode data")
+            }
+            
+        } catch {
+            print("Error \(error.localizedDescription)")
+        }
+    }
+    
 }
 
 #Preview {
