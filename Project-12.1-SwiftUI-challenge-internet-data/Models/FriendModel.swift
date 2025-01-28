2971585db4d507a30a72be5b81fcbe3285147dae
kevin cuadros <kevindavidcuadros@gmail.com>
2025-01-28 16:16:03 -0500

    create models

---
+++ Project-12.1-SwiftUI-challenge-internet-data/Models/FriendModel.swift
@@ -0,0 +1,13 @@
+//
+//  FriendModel.swift
+//  Project-12.1-SwiftUI-challenge-internet-data
+//
+//  Created by Kevin Cuadros on 28/01/25.
+//
+
+import Foundation
+
+struct FriendModel: Codable {
+    var id: String
+    var name: String
+}
---
+++ Project-12.1-SwiftUI-challenge-internet-data/Models/UserModel.swift
@@ -0,0 +1,20 @@
+//
+//  UserModel.swift
+//  Project-12.1-SwiftUI-challenge-internet-data
+//
+//  Created by Kevin Cuadros on 28/01/25.
+//
+
+import Foundation
+
+
+struct UserModel: Codable {
+    let id: String
+    let isActive: Bool
+    let name: String
+    let age: Int
+    let company, email, address, about: String
+    let registered: Date
+    let tags: [String]
+    let friends: [FriendModel]
+}
